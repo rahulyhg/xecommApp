ef9b4247db575c7d901b412bad8d34609f4a0249
Gowrav Vishwakarma <gowrav@xserver.(none)>
2014-07-13 11:42:13 +0530

    Categories join to left join

--- page/owner/category.php
+++ page/owner/category.php
@@ -5,7 +5,15 @@ class page_xecommApp_page_owner_category extends page_xecommApp_page_owner_main{
 	function init(){
 		parent::init();
 
+		$category_model = $this->add('xecommApp/Model_Category');
+		$category_model->title_field='category_name';
+
 		$crud=$this->add('CRUD');
-		$crud->setModel('xecommApp/Category');
+		$crud->setModel($category_model);
+		if($crud->form){
+			$parent_model = $crud->form->getElement('parent_id')->getModel();
+			$parent_model->title_field='category_name';
+			// $parent_model->debug();
+		}
 	}
 }
