34dfa5d43a4b6f57e47e5884d4e03581f393ba1f
Gowrav Vishwakarma <gowrav@xserver.(none)>
2014-07-14 10:12:31 +0530

    category and email template changed a bit

--- lib/Model/Category.php
+++ lib/Model/Category.php
@@ -2,17 +2,17 @@
 namespace XecommApp;
 
 class Model_Category extends \Model_Table{
-	var $table="xecomm_categories";
+	var $table="xecommApp_categories";
 	var $table_alias = 'category';
 	function init(){
 		parent::init();
-		$this->hasOne('xecommApp/ParentCategory','parent_id');
+		$this->hasOne('xecommApp/ParentCategory','parent_id')->defaultValue(0);
 
 		$this->addField('name')->Caption('Category Name');
 		$this->hasMany('xecommApp/Category','parent_id',null,'SubCategories');
 		$this->hasMany('xecommApp/Product','category_id');
 
-		$parent_join = $this->leftJoin('xecomm_categories','parent_id');
+		$parent_join = $this->leftJoin('xecommApp_categories','parent_id');
 
 		$this->addExpression('category_name')->set('concat('.$this->table_alias.'.name,"- (",IF('.$parent_join->table_alias.'.name is null,"",'.$parent_join->table_alias.'.name),")")');
 
