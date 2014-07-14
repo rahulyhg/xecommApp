0ec33d1b7db7e83f3102d9cd14275f2ddd32f705
Ayushi Bhanwra <ayushi@ayushi-COMPAQ-PRESARIO-CQ62-103TU.(none)>
2014-07-14 20:10:04 +0530

    modified this file

--- templates/js/xecomm-checkout5.js
+++ templates/js/xecomm-checkout5.js
@@ -20,6 +20,31 @@ $.each({
       $(this).univ().errorMessage('Points not entered correctly ');
     }
     $(net_field).val(($(total_field).val()*1) - ($(points_redeemed_field).val()*1 / points_req_for_one_rupees));
+
+  },
+  validateVoucher:function(voucher_field_id, form_id, discount_field_id, net_field_id){
+      // $.univ().ajaxec('index.php?page=xecommApp_page_ajaxhandler&cut_page=1&task=validateVoucher&v_no='+$(voucher_no).val()+'&subpage=xecomm-junk');      
+      if($(voucher_field_id).val()=="") return;
+      $.ajax({
+            url: 'index.php?page=xecommApp_page_ajaxhandler&cut_page=1&task=validateVoucher&v_no='+$(voucher_field_id).val()+'&subpage=xecomm-junk',
+            type: 'GET',
+            data: {
+              form : form_id,
+              voucher_field: $(voucher_field_id).attr('name'),
+              discount_field: discount_field_id, 
+              net_field: net_field_id
+            }
+          })
+          .done(function(ret) {
+            eval(ret);
+            console.log(ret);
+          })
+          .fail(function() {
+            $(this).univ().errorMessage('Oops, Activity was not edited');
+          })
+          .always(function() {
+            console.log("complete");
+          });
   }
 
 },$.univ._import);
